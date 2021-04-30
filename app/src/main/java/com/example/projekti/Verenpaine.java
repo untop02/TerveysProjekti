ed87569932bd1ea9c6bdbf6b6b811009d9f068b5
Sylvester Salo <800003232@omnia.fi>
2021-04-30 12:46:52 +0300

    Verenpainelaskuri V0.3

--- app/src/main/java/com/example/projekti/Verenpaine.java
+++ app/src/main/java/com/example/projekti/Verenpaine.java
@@ -10,8 +10,7 @@ import android.widget.TextView;
 
 public class Verenpaine extends AppCompatActivity {
 
-
-    TextView verenpaineKentta;
+    TextView verenpaineKentta, verenpaineInfo;
     Button laskeBtn;
 
     @Override
@@ -20,10 +19,15 @@ public class Verenpaine extends AppCompatActivity {
         setContentView(R.layout.activity_verenpaine);
 
         verenpaineKentta = findViewById(R.id.tvVerenpaine);
+        verenpaineInfo = findViewById(R.id.tvVerenpaineInfo);
         laskeBtn = findViewById(R.id.laskeBtn);
         EditText ylaArvo = findViewById(R.id.ylaArvo);
         EditText alaArvo = findViewById(R.id.alaArvo);
 
+        String normaaliVp = ("Verenpaine on normaali, kun se on alle 130/85 mmHg.");
+        String tyydyttavaVp = ("Verenpaine on tyydyttävä, kun se on 130-139/ 85-89 mmHg välillä.");
+        String korkeaVp = ("Verenpaine on koholla, kun se on yli 140/90 mmHg.");
+        String tyhja = ("");
 
         laskeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,10 +35,18 @@ public class Verenpaine extends AppCompatActivity {
                 int yla = Integer.parseInt(ylaArvo.getText().toString());
                 int ala = Integer.parseInt(alaArvo.getText().toString());
 
-                if (yla >= 140 && ala > 90) {
+                if (yla >= 139 && ala >= 89) {
                     verenpaineKentta.setText("Verenpaineesi on koholla.");
-                } else {
-                    verenpaineKentta.setText("Verenpaineesi on nomaali");
+                    verenpaineInfo.setText(korkeaVp);
+                } else if(yla >= 129 && yla <= 139 && ala >= 85 && ala <= 89) {
+                    verenpaineKentta.setText("Verenpaineesi on tyydyttävä.");
+                    verenpaineInfo.setText(tyydyttavaVp);
+                } else if(yla <= 130 && ala <= 85) {
+                    verenpaineKentta.setText("Verenpaineesi on normaali.");
+                    verenpaineInfo.setText(normaaliVp);
+                } else if (yla >= 159 || yla <= 99 || ala >= 119 || ala <= 69) {
+                    verenpaineKentta.setText("Anna sopiva luku.");
+                    verenpaineInfo.setText(tyhja);
                 }
             }
         });
