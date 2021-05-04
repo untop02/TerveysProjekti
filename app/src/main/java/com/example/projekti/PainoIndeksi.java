690147cd9a1dd30128c3a365a227e117a05645c0
BuildTools <unconfigured@null.spigotmc.org>
2021-05-04 12:07:55 +0300

    PainoIndeksi proto

--- app/src/main/java/com/example/projekti/PainoIndeksi.java
+++ app/src/main/java/com/example/projekti/PainoIndeksi.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PainoIndeksi extends AppCompatActivity {
 
@@ -23,23 +24,31 @@ public class PainoIndeksi extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int w = Integer.parseInt(paino.getText().toString());
-                double h = Double.parseDouble(pituus.getText().toString())/100;
-                double bmi = w/(h*h);
-                double roundDbl = Math.round(bmi*100.0)/100.0;
-                String stringBmi = Double.toString(roundDbl);
-                if(bmi<18.5){
-                    tv.setText("Paino indeksisi on "+stringBmi +" tämä määritellään alipainoiseksi.");
+                try{
+                    int w = Integer.parseInt(paino.getText().toString());
+                    double h = Double.parseDouble(pituus.getText().toString())/100;
 
-                }
-                if(bmi>=18.5 && bmi <= 24.9){
-                    tv.setText("Paino indeksisi on "+stringBmi +" tämä on terve painoindeksi.");
+                    double bmi = w/(h*h);
+                    double roundDbl = Math.round(bmi*100.0)/100.0;
+                    String stringBmi = Double.toString(roundDbl);
+                    if(bmi<18.5){
+                        tv.setText("Paino indeksisi on "+stringBmi +" tämä määritellään alipainoiseksi.");
 
-                }
-                if(bmi > 25){
-                    tv.setText("Paino indeksisi on "+stringBmi +" tämä määritellään ylipainoiseksi.");
+                    }
+                    if(bmi>=18.5 && bmi <= 24.9){
+                        tv.setText("Paino indeksisi on "+stringBmi +" tämä on terve painoindeksi.");
 
+                    }
+                    if(bmi > 25){
+                        tv.setText("Paino indeksisi on "+stringBmi +" tämä määritellään ylipainoiseksi.");
+
+                    }
+                }catch (NumberFormatException exception) {
+                    Toast.makeText(getApplicationContext(), "Täytä kentät", Toast.LENGTH_SHORT).show();
                 }
+
+
+
 
             }
         });
