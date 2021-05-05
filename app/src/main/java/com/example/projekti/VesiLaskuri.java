158ca08faf4c368c405bdd5d1b8edc6dea85b70e
Aksu <akselias02@gmail.com>
2021-05-05 12:21:44 +0300

    Vesilaskuri Versio 0.9

--- app/src/main/java/com/example/projekti/VesiLaskuri.java
+++ app/src/main/java/com/example/projekti/VesiLaskuri.java
@@ -2,18 +2,23 @@ package com.example.projekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class VesiLaskuri extends AppCompatActivity {
 
-    double vesimaara;
+    Double vesimaara;
     TextView vedenmaara;
     Button pienipullo;
     Button isopullo;
     Button vesilasi;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String vesitalletus = "vesiavain";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +51,45 @@ public class VesiLaskuri extends AppCompatActivity {
     }
     public void lisaaLasi(){
         vesimaara += 0.3;
-        vedenmaara.setText(String.valueOf(vesimaara));
+        double roundOff = Math.round(vesimaara * 100.0) / 100.0;
+        vedenmaara.setText(String.valueOf(roundOff));
     }
     public void lisaapienipullo(){
         vesimaara += 0.5;
-        vedenmaara.setText(String.valueOf(vesimaara));
+        double roundOff = Math.round(vesimaara * 100.0) / 100.0;
+        vedenmaara.setText(String.valueOf(roundOff));
     }
     public void lisaaisopullo(){
         vesimaara += 1.5;
-        vedenmaara.setText(String.valueOf(vesimaara));
+        double roundOff = Math.round(vesimaara * 100.0) / 100.0;
+        vedenmaara.setText(String.valueOf(roundOff));
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadData();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        talletus();
+    }
+    public void talletus(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(vesitalletus, vedenmaara.getText().toString());
+        editor.apply();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String text = sharedPreferences.getString(vesitalletus, "");
+
+        TextView vedenmaara = findViewById(R.id.juotuMaara);
+        vedenmaara.setText(text);
+    }
+
 
 }
