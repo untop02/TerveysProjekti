d8d8895dd1a23e8fa3e254180b0a1c4a5ed8ee8b
Aksu <akselias02@gmail.com>
2021-05-05 23:28:11 +0300

    Vesilaskuri Versio 1.2

--- app/src/main/java/com/example/projekti/VesiLaskuri.java
+++ app/src/main/java/com/example/projekti/VesiLaskuri.java
@@ -29,33 +29,34 @@ public class VesiLaskuri extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vesi_laskuri);
 
-
-
         vedenmaara = findViewById(R.id.juotuMaara);
         vesilasi = findViewById(R.id.lasiBtn);
         isopullo = findViewById(R.id.isopulloBtn);
         pienipullo = findViewById(R.id.pienipulloBtn);
         nollaa = findViewById(R.id.resetBtn);
 
+        //nappula joka lisää tietyn määrän vettä
         nollaa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 reset();
             }
-
         });
+        //nappula joka lisää tietyn määrän vettä
         vesilasi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 lisaaLasi();
             }
         });
+        //lisää tietyn määrän vettä
         pienipullo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 lisaapienipullo();
             }
         });
+        //nappula joka lisää tietyn määrän vettä
         isopullo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,42 +64,50 @@ public class VesiLaskuri extends AppCompatActivity {
             }
         });
     }
+    //resetointi metodi
     public void reset(){
         vesimaara = 0.0;
         TextView vedenmaara = findViewById(R.id.juotuMaara);
         vedenmaara.setText("0.0");
     }
+    //Metodi jonka kutsuessa lisää tietyn määrän vettä
     public void lisaaLasi(){
         vesimaara += 0.3;
         double roundOff = Math.round(vesimaara * 100.0) / 100.0;
         vedenmaara.setText(String.valueOf(roundOff));
     }
+    //Metodi jonka kutsuessa lisää tietyn määrän vettä
     public void lisaapienipullo(){
         vesimaara += 0.5;
         double roundOff = Math.round(vesimaara * 100.0) / 100.0;
         vedenmaara.setText(String.valueOf(roundOff));
     }
+    //Metodi jonka kutsuessa lisää tietyn määrän vettä
     public void lisaaisopullo(){
         vesimaara += 1.5;
         double roundOff = Math.round(vesimaara * 100.0) / 100.0;
         vedenmaara.setText(String.valueOf(roundOff));
     }
+    //kutsuu käynnistyksen kohdalla loadDataa
     @Override
     protected void onStart() {
         super.onStart();
         loadData();
     }
+    //käyttäjän poistuessa sovelluksesta kutsuu talletusta
     @Override
     protected void onPause() {
         super.onPause();
         talletus();
     }
+    //tallentaa tiedot
     public void talletus(){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(vesitalletus, vedenmaara.getText().toString());
         editor.apply();
     }
+    //lataa tiedot
     public void loadData() {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         String text = sharedPreferences.getString(vesitalletus, "");
