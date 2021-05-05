90b57b184f1fafa903008ebae0ba4cce9812f34c
BuildTools <unconfigured@null.spigotmc.org>
2021-05-05 11:04:44 +0300

    ui muutos ja tallentuva painoindeksi babyyy

--- app/src/main/java/com/example/projekti/PainoIndeksi.java
+++ app/src/main/java/com/example/projekti/PainoIndeksi.java
@@ -1,8 +1,11 @@
 package com.example.projekti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +14,17 @@ import android.widget.Toast;
 
 public class PainoIndeksi extends AppCompatActivity {
 
+    private static String PROVIDER = "search_manager";
+    private static String QUERY = "query";
+    private static final String KEY = "painoAvain";
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+
+    int w;
+    double h;
+    String stringBmi;
+
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,18 +33,18 @@ public class PainoIndeksi extends AppCompatActivity {
         Button b = (Button) findViewById(R.id.laske);
         EditText paino = (EditText) findViewById(R.id.Paino);
         EditText pituus = (EditText) findViewById(R.id.Pituus);
-        TextView tv = (TextView) findViewById(R.id.textView);
-
+         tv = (TextView) findViewById(R.id.textView);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try{
-                    int w = Integer.parseInt(paino.getText().toString());
-                    double h = Double.parseDouble(pituus.getText().toString())/100;
+                    w = Integer.parseInt(paino.getText().toString());
+                    h = Double.parseDouble(pituus.getText().toString())/100;
 
                     double bmi = w/(h*h);
                     double roundDbl = Math.round(bmi*100.0)/100.0;
-                    String stringBmi = Double.toString(roundDbl);
+                    stringBmi = Double.toString(roundDbl);
+
                     if(bmi<18.5){
                         tv.setText("Paino indeksisi on "+stringBmi +" tämä määritellään alipainoiseksi.");
 
@@ -51,7 +65,48 @@ public class PainoIndeksi extends AppCompatActivity {
 
 
             }
+
         });
+
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+
+        loadData();
+
+        Log.d(null, "onStart() kutsuttu");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        saveData();
+
+    }
+   public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY, tv.getText().toString());
+        editor.apply();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String text = sharedPreferences.getString(KEY, "");
+
+
+
+
+        TextView tv = findViewById(R.id.textView);
+        tv.setText(text);
+
+
+
     }
 
 }
